6fafe04c61b7ec356f1c5c889b6cc07c9cbe549b
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-10-08 23:24:53 -0700

    name gone

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -46,17 +46,7 @@ export default function SignIn({
                 type="hidden"
                 defaultValue={csrfToken || ""}
               />
-              <label>
-                Name
-                <input
-                  type="name"
-                  id="name"
-                  name="name"
-                  autoComplete="name"
-                  required
-                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </label>
+              
               <label>
                 Email address
                 <input
