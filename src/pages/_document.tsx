ae656eee1abd5c508c6338f050310587e8006ee1
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-10-08 22:35:21 -0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
     </Head>
       <body>
         <Main />
