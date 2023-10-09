b4102dc3fc3f0bbea73ef31a8441fc0439957cde
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-10-08 22:33:52 -0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="apple-mobile-web-app-capable" content="yes">
+    </Head>
       <body>
         <Main />
         <NextScript />
