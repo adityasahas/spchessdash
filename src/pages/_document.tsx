658b64b9ccf69976773a8323230ca9c295479dfa
Aditya Sahasranam <adityasahas2025@gmail.com>
2023-10-09 00:13:01 -0700

    maybe

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-        <meta name="apple-mobile-web-app-capable" content="yes"/>
-    </Head>
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1; viewport-fit=cover"
+        />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
